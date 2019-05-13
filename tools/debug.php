6b732fe78c9274f6f7db63e239c3a8e40134de39
sachyya <sachyya1@gmail.com>
2019-05-13 12:19:08 +0200

    Check if function is defined in other plugins too.

--- tools/debug.php
+++ tools/debug.php
@@ -1,12 +1,16 @@
 <?php
-function gh_print( $var, $color="red" ) {
-	echo '<pre style="color:' . esc_attr( $color ) . '">';
-	print_r( $var );
-	echo '</pre>';
+if ( ! function_exists( 'gh_print' ) ) {
+	function gh_print( $var, $color="red" ) {
+		echo '<pre style="color:' . esc_attr( $color ) . '">';
+		print_r( $var );
+		echo '</pre>';
+	}
 }
 
-function gh_dump( $var, $color="red" ) {
-	echo '<pre style="color:' . esc_attr( $color ) . '">';
-	var_dump( $var );
-	echo '</pre>';
+if ( ! function_exists( 'gh_dump' ) ) {
+	function gh_dump( $var, $color="red" ) {
+		echo '<pre style="color:' . esc_attr( $color ) . '">';
+		var_dump( $var );
+		echo '</pre>';
+	}
 }
--- tools/rm-theme-mods.php
+++ tools/rm-theme-mods.php
@@ -1,24 +1,24 @@
 <?php
-function gh_add_admin_item() {
-    global $wp_admin_bar;
+// function gh_add_admin_item() {
+//     global $wp_admin_bar;
 
-    if ( ! is_super_admin() || ! is_admin_bar_showing() )
-        return;
+//     if ( ! is_super_admin() || ! is_admin_bar_showing() )
+//         return;
 
-    // Add the item to admin bar.
-    $wp_admin_bar->add_menu( 
-    	array( 
-	    	'id' => 'gd-remove-theme-mods', 
-	    	'title' => __( 'Remove Theme Mods', 'ghoul-debug' ), 
-	    	'href' => add_query_arg( 'gh-remove-theme-mods', true, wp_customize_url() ),
-	    ) 
-    );
+//     // Add the item to admin bar.
+//     $wp_admin_bar->add_menu( 
+//     	array( 
+// 	    	'id' => 'gd-remove-theme-mods', 
+// 	    	'title' => __( 'Remove Theme Mods', 'ghoul-debug' ), 
+// 	    	'href' => add_query_arg( 'gh-remove-theme-mods', true, wp_customize_url() ),
+// 	    ) 
+//     );
     
-    // If the request is done, remove all theme mods.
-    if ( isset( $_GET['gh-remove-theme-mods'] ) && $_GET['gh-remove-theme-mods'] ) {
-    	remove_theme_mods();
-    } else {
-    	return;
-    }
-}
-add_action( 'admin_bar_menu', 'gh_add_admin_item', 1000 );
+//     // If the request is done, remove all theme mods.
+//     if ( isset( $_GET['gh-remove-theme-mods'] ) && $_GET['gh-remove-theme-mods'] ) {
+//     	remove_theme_mods();
+//     } else {
+//     	return;
+//     }
+// }
+// add_action( 'admin_bar_menu', 'gh_add_admin_item', 1000 );
